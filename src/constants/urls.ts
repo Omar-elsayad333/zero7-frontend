133796f60514faff0320c026020b82d90859c894
Omar-elsayad333 <omarelsayad333@gmail.com>
2023-11-24 21:25:35 +0200

    main

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,13 +1,13 @@
 export default class Urls {
-    // User urls
-    static login: string = '/user/login'
-    static register: string = '/user/signup'
-    static userData: string = '/user/userData'
-    static refreshToken: string = '/user/refreshToken'
-    static resetPassword: string = '/user/resetPassword'
+  // User urls
+  static login: string = '/user/login'
+  static register: string = '/user/signup'
+  static userData: string = '/user/userData'
+  static refreshToken: string = '/user/refreshToken'
+  static resetPassword: string = '/user/resetPassword'
 
-    // Product urls
-    static products: string = '/products'
-    static genders: string = '/products/genders'
-    static categorys: string = '/products/categorys'
+  // Product urls
+  static products: string = '/products'
+  static genders: string = '/products/genders'
+  static categorys: string = '/products/categorys'
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
-import App from './App';
+import App from './App'
 import 'styles/global.css'
-// import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { UserProvider } from 'contexts/userContext';
+import ReactDOM from 'react-dom/client'
+import { UserProvider } from 'contexts/userContext'
 
-const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-)
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-    // <React.StrictMode>
-        <UserProvider>
-            <App />
-        </UserProvider>
-    // </React.StrictMode>
+  <UserProvider>
+    <App />
+  </UserProvider>,
 )
